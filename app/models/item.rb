f856266c6b10432a02d41f5b2f5d57ccc0846e15
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-09-04 20:10:24 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :user
+  belongs_to_active_hash :genre
+  has_one_attached :image
+
+  validates :name, :content, :genre, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :genre_id
+  end
 end
