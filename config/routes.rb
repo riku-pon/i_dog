edc8beed67e8fa01002934bcfa45cac3f9dd1733
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-09-08 08:59:20 +0900

    商品詳細機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items
   root 'items#index'
+<<<<<<< Updated upstream
+=======
+  resources :items, only: [:index, :new, :create]
+  resources :dog_items, only: [:index, :show]
+>>>>>>> Stashed changes
 end
