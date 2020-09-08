5934467e045c5bd5fc08be6ea16866d42b6f9c39
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-09-08 09:07:22 +0900

    CSSの適応

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items, only: [:index, :new, :create]
-end
+  resources :dog_items, only: [:index, :show]
